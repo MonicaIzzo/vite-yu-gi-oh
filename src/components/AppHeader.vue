a8dbf0c93396bbd921ece2a9766f0a750eff8d19
MonicaIzzo <info@monicaizzo.net>
2023-07-09 11:20:38 +0200

    filter debug 2

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,10 +8,7 @@ export default {
     // logica qui
     components: { BaseSelect },
     data() {
-        return {
-            store,
-            types
-        }
+        return store
     }
 
 };
@@ -21,9 +18,7 @@ export default {
 <template>
     <header>
         <div class="filter">
-            <BaseSelect defaultLabel="Tutti i tipi"
-                :options='[
-                    "Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water"]' />
+            <BaseSelect />
         </div>
     </header>
 </template>
